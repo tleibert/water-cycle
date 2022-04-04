5d7f11f7f32a771d6312fe16281fd2a8cd871f01
tmleibe2 <tmleibe2@ncsu.edu>
2022-04-03 20:35:48 -0400

    Get some good work done on the CLI

--- water_cycle/main.py
+++ water_cycle/main.py
@@ -2,20 +2,104 @@
 Main file of the Water cycle simulation.
 """
 
-from water_cycle.model import World
+import sys
+
+from rich.console import Console
+from PyInquirer import prompt
+
+from water_cycle.model import World, Location, Flux
+
+console = Console()
 
 
-def run_game(starting_location, world: World):
+def ask_next_action():
     """
-    Runs the game's main loop
+    Top-level prompt for a step through the game.
+    The actions a user can take at the top-level are viewing
+    the possible outflows from their stage in the water cycle,
+    learning more about their stage in the water cycle, and
+    exiting the application.
     """
 
-    current_location = starting_location
+    action_prompt = {
+        "type": "list",
+        "name": "action",
+        "message": "What would you like to do?",
+        "choices": ["view description", "choose outflow", "quit"],
+    }
+    answers = prompt(action_prompt)
+    return answers["action"]
+
+
+def view_description(location: Location):
+    """
+    Gets the detailed information
+    """
+    console.print(f"[green]{location.name}[/green]")
+    data = location.info
+    if data:
+        console.print(
+            f"Total pool size and certainty: [cyan]{data}[/cyan] "
+            "[dim](in 1000s of cubic kilometers of water)[/dim]\n"
+        )
+
+    console.print("Description:")
+    console.print(location.description)
+    input()
+
+
+def choose_outflow(location: Location) -> Flux:
+    fluxes = location.outflows
+    flux_name_map = {flux.name: flux for flux in fluxes}
+
+    flux_prompt = {
+        "type": "list",
+        "name": "flux",
+        "message": f"Possible outflows from {location.name}:",
+        "choices": list(flux_name_map.keys()),
+    }
+
+    answers = prompt(flux_prompt)
+    return flux_name_map[answers["flux"]]
+
+
+def game_step(world: World, location: Location) -> Location:
+    """
+    Runs an iteration the game's main loop.
+
+    Returns true if the game should step again
+    """
+
+    print(f"Your location: {location.name}")
+    action = ask_next_action()
+    if action == "quit":
+        print("Goodbye!")
+        sys.exit(0)
+    elif action == "view description":
+        view_description(location)
+    elif action == "choose outflow":
+        # get the flux corresponding to the chosen outflow
+        flux = choose_outflow(location)
+
+        
+    else:
+        raise ValueError(f"Unknown action {action}")
+
+    return location
+
+
+def start_interactive(world: World):
+    """
+    Runs the interactive loop of the simulation.
+    """
+
+    location = world.random_location()
+
     while True:
-        print(f"You are in {current_location}")
-        user_input = input("What do you want to do?\n")
-        if user_input is None or user_input.lower().startswith("q"):
-            return
+        new_location = game_step(world, location)
+        if new_location is not location:
+            console.clear()
+            location = new_location
 
 
 def main():
@@ -23,13 +107,13 @@ def main():
     Starts the program
     """
     world = World.from_config("water_cycle/resources/world.yaml")
-    print("Welcome to the Water Cycle!")
-    print(world)
-    start = world.random_location()
-    print(f"You are starting in: {start.name}")
-    print("You have the following fluxes available:")
-    print(f"{start.outflows}")
-    input()
+    console.print("Welcome to the Water Cycle!")
+
+    try:
+        start_interactive(world)
+    except EOFError:
+        pass  # exit gracefully
+
     print("Goodbye!")
 
 
